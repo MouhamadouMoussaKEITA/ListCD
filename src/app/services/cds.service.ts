db3d1674a20343e712768f2c70239e78311a0f30
Mouhamadou Keita <mkmouhamadou@iutbayonne.univ-pau.fr>
2025-03-31 19:17:25 +0200

    Etape 7 partiellement completé

--- src/app/services/cds.service.ts
+++ src/app/services/cds.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CD } from '../Models/cd.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +19,29 @@ export class CdsService {
     return this.http.get<CD>('https://localhost:3000/CD/' + id);
   }
 
+  addCD(nouvCD: CD): Observable<CD> {
+
+
+    this.getCds().subscribe(cds => {
+      let maxId = 0;
+      cds.forEach(cd => { maxId = (cd.id > maxId ? cd.id : maxId); });
+      nouvCD.id = maxId + 1;
+      this.http.post<CD>('https://localhost:3000/CD', nouvCD);
+    }
+    );
+
+    return this.getCds().pipe(
+      switchMap(cds => {
+        let maxId = 0;
+        cds.forEach(cd => { maxId = (cd.id > maxId ? cd.id : maxId); });
+        nouvCD.id = maxId + 1;
+        return this.http.post<CD>('https://localhost:3000/CD', nouvCD);
+      }
+      ));
+
+  };
 }
+
+
+
+
